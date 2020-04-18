f71017ade62ea7fc3a2149298aeb1c85ed598b30
Alan Vieceli <alan.vieceli@softplan.com.br>
2020-04-18 11:19:31 -0300

    EntityBase e Builder

---
+++ src/Microservice.Whatevers.Domain/Abstractions/EntityBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using Microservice.Whatevers.Domain.Abstractions.Notifications;
+
+namespace Microservice.Whatevers.Domain.Abstractions
+{
+    public abstract class EntityBase<TId>
+        where TId : struct
+    {
+        public readonly INotification Notification = new Notification();
+        public TId Id { get; protected set; }
+
+        public bool IsValid() => Notification.GetErrors()?.Any() == false;
+
+        protected abstract void SetId(TId id);
+    }
+}
